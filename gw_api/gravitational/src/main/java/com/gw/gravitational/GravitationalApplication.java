961b6771477d7294cf9f80276257d645ae19b57d
Gauthamarcot <gauthamarcot>
2020-10-08 09:05:46 -0400

    api init

---
+++ gw_api/gravitational/src/main/java/com/gw/gravitational/GravitationalApplication.java
@@ -0,0 +1,13 @@
+package com.gw.gravitational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravitationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravitationalApplication.class, args);
+	}
+
+}
